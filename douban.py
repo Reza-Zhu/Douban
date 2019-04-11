ec03ce6904f3e0e0a3a3daa10a66006549c1bae4
a101015213 <101015213@qq.com>
2019-04-11 15:31:46 +0800

    Douban V1.3

--- douban.py
+++ douban.py
@@ -5,24 +5,27 @@ import jieba
 import wordcloud
 from count import Count
 from stopword import movestopwords
+import os
 num=0
-for nums in range(10):
-    URL='https://movie.douban.com/subject/1291546/comments?start=%s&limit=20&sort=time&status=P'%num
-    html=requests.get(URL).text
-    soup=BeautifulSoup(html,'lxml')
-    img_ul=soup.find_all('span',{'class':'short'})
-    num=num+20
-    for cm in img_ul:
-        pattern_description = re.compile('<span class="short">([\s\S]+)</span>')
-        list_description = re.findall(pattern_description,str(cm))
-        #print(list_description)
-        with open('comment1.txt','a',encoding='utf-8') as f:
-            f.write(list_description[0])
-            f.close()
-txt=open('comment1.txt',encoding='utf-8').read()
+
+if not os.path.exists('.\comment.txt'):
+    for nums in range(10):
+        URL='https://movie.douban.com/subject/1291546/comments?start=%s&limit=20&sort=time&status=P'%num
+        html=requests.get(URL).text
+        soup=BeautifulSoup(html,'lxml')
+        img_ul=soup.find_all('span',{'class':'short'})
+        num=num+20
+        for cm in img_ul:
+            pattern_description = re.compile('<span class="short">([\s\S]+)</span>')
+            list_description = re.findall(pattern_description,str(cm))
+
+            with open('comment.txt','a',encoding='utf-8') as f:
+                f.write(list_description[0])
+                f.close()
+txt=open('comment.txt',encoding='utf-8').read()
 res=movestopwords(txt)
 w=wordcloud.WordCloud(width=1000,font_path='C:\Windows\Fonts\msyh.ttf',height=700)
 listword=jieba.lcut(res)
 w.generate(" ".join(listword))
 w.to_file("new.png")
-Count('comment1.txt','的','也','太')
+Count('comment.txt','的','也','太')
