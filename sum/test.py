a90bee2367c2589015c55280f189bf174b7efa2f
RoRoKa <101015213@qq.com>
2019-05-31 19:59:56 +0800

    Delete test.py

--- sum/test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-window=tk.Tk()
-window.title(' ')
-window.geometry('400x400')
-img = tk.PhotoImage(file='./阿甘正传.gif')
-l_img = tk.Label(window, image=img)
-l_img.pack()
-window.mainloop()
